e6003f3c547850966a063406885e1e5ceaed6fee
Hatim <sakazokihatim@gmail.com>
2025-02-21 17:18:03 +0100

    modification

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const productRoutes = require("./routes/products");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = 9000;
 
 app.use(cors());
 app.use(express.json());
